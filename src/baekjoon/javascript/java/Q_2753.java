e9746eb5b9ddf61b910302fb9b72c9127f1dad01
이바다 <gnjqtpf1@gmail.com>
2023-11-19 07:29:16 +0900

    JAVA 백준 2753 문제(윤년)

--- src/baekjoon/javascript/java/Q_2753.java
+++ src/baekjoon/javascript/java/Q_2753.java
@@ -1,20 +1,22 @@
-import java.util.Scanner;
+package baekjoon.javascript.java;
 
-public class Main {
+import java.util.*;
+import java.io.*;
+
+public class Q_2753 {
     static Scanner scn = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  throws IOException{
         // 연도가 주어졌을 때, 윤년이면 1, 아니면 0을 출력하는 프로그램
         // 연도가 4의 배수이면서, 100의 배수가 아닐 때 또는 400의 배수일 때다.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int year = Integer.parseInt(br.readLine());
 
-        System.out.println("연도를 입력하세요");
-        int target_year = scn.nextInt();
-
-        System.out.println(cal_result(target_year));
+        System.out.println(cal_result(year));
     }
 
+    // 윤년 계산
     static int cal_result(int year){
-
         if(  (year % 4 == 0 && year % 100 != 0) ||  year % 400 == 0){
             return 1;
         } else {
